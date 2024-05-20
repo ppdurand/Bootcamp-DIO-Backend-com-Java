31c1f38f5cd201765291ee76ef7669cecea02040
Pedro Paulo Durand <pedropaulodurand@gmail.com>
2024-05-20 20:50:28 -0300

    desafios 11 - 19

--- StremAPI-Desafios/src/main/java/edu/PedroDurand/Main.java
+++ StremAPI-Desafios/src/main/java/edu/PedroDurand/Main.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3, 11, 15, 30, 45);
         MetodosAux metodosAux = new MetodosAux();
 
         //Desafio 1 - Mostre a lista na ordem numérica:
@@ -45,6 +45,37 @@ public class Main {
 
         //Desafio 10 - Agrupe os valores ímpares múltiplos de 3 ou de 5:
         System.out.println(numeros.stream().filter(n -> n % 2 != 0).filter(n -> n % 3 == 0 || n % 5 == 0).toList());
+
+        //Desafio 11 - Encontre a soma dos quadrados de todos os números da lista:
+        System.out.println(numeros.stream().mapToInt(metodosAux::quadrado).sum());
+
+        //Desafio 12 - Encontre o produto de todos os números da lista:
+        System.out.println(numeros.stream().mapToLong(Integer::longValue).reduce((a, b) -> a * b));
+
+        //Desafio 13 - Filtrar os números que estão dentro de um intervalo:
+        System.out.println(numeros.stream().filter(n -> n > 5 && n < 10).toList());
+
+        //Desafio 14 - Encontre o maior número primo da lista:
+        System.out.println(numeros.stream().filter(metodosAux::primo).max(Integer::compareTo)
+                .orElse(null));
+
+        //Desafio 15 - Verifique se a lista contém pelo menos um número negativo:
+        System.out.println(numeros.stream().anyMatch(n -> n < 0));
+
+        //Desafio 16 - Agrupe os números em pares e ímpares:
+        System.out.println(numeros.stream().filter(n -> n % 2 == 0).toList());
+        System.out.println(numeros.stream().filter(n -> n % 2 != 0).toList());
+
+        //Desafio 17 - Filtrar os números primos da lista:
+        System.out.println(numeros.stream().filter(metodosAux::primo).distinct().toList());
+
+        //Desafio 18 - Verifique se todos os números da lista são iguais:
+        System.out.println(numeros.stream().distinct().count() == 1);
+
+        //Desafio 19 - Encontre a soma dos números divisíveis por 3 e 5:
+        System.out.println(numeros.stream().filter(n -> n % 3 == 0 && n % 5 ==0)
+                .mapToInt(Integer::intValue)
+                .sum());
         
     }
 }
--- StremAPI-Desafios/src/main/java/edu/PedroDurand/MetodosAux.java
+++ StremAPI-Desafios/src/main/java/edu/PedroDurand/MetodosAux.java
@@ -9,4 +9,20 @@ public class MetodosAux {
         }
         return soma;
     }
+    public Integer quadrado(Integer numero){
+      Integer quadrado;
+      return quadrado = numero * numero;
+    }
+
+    public boolean primo(Integer numero){
+        if (numero <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
